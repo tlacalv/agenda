37abc45dbf94cdeb40a0b03663d53003fadb88fa
Tlacaelel <radeon777@live.com>
2021-05-17 20:35:46 -0600

    Added icons

--- src/components/Agenda/components/ToolBar.js
+++ src/components/Agenda/components/ToolBar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { setToday, previousWeek, nextWeek, getMonth, getYear} from '../utils'
+import Icon from '@mdi/react'
+import {mdiChevronLeft, mdiChevronRight} from '@mdi/js'
+
 export default function ToolBar({date, setDate}) {
   return (
     <div className="tools">
       <div className="controls">
         <button onClick={() => setToday(setDate)}>Hoy</button>
-        <button onClick={() => previousWeek(date, setDate)}>{"<"}</button>
-        <button onClick={() => nextWeek(date, setDate)}>{">"}</button>
+        <button onClick={() => previousWeek(date, setDate)}><Icon path={mdiChevronLeft} size={1} /></button>
+        <button onClick={() => nextWeek(date, setDate)}><Icon path={mdiChevronRight} size={1} /></button>
       </div>
       <div className="month">
         {getMonth(date)} {getYear(date)}
