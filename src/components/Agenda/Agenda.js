5777eafb0b707aad55451a7a6a38bd190e36d1a6
Tlacaelel <radeon777@live.com>
2021-05-24 15:19:37 -0600

    Added styles to display events

--- src/components/Agenda/Agenda.js
+++ src/components/Agenda/Agenda.js
@@ -1,15 +1,20 @@
-import {useState} from 'react';
+import { useState } from "react";
 import Week from "./components/Week";
 import ToolBar from "./components/ToolBar";
 import styles from "./sass/agenda.module.scss";
 
 export default function Calendar({ date, setDate }) {
-  const [events, setEvents] = useState([])
-  console.log(events)
+  const [events, setEvents] = useState([]);
+  console.log("main state, ", events);
   return (
     <div className={styles.agenda}>
       <ToolBar date={date} setDate={setDate} />
-      <Week date={date} events={events} setEvents={setEvents} setDate={setDate} />
+      <Week
+        date={date}
+        events={events}
+        setEvents={setEvents}
+        setDate={setDate}
+      />
     </div>
   );
 }
--- src/components/Agenda/components/Event.js
+++ src/components/Agenda/components/Event.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import styles from '../sass/event.module.scss';
+import React from "react";
+import styles from "../sass/event.module.scss";
 
-const eventStyle = (start, end) => {
-  const startMinutes = (start.get('hour')*60) + start.get('minute')
-  const endMinutes = (end.get('hour')*60) + end.get('minute')
+const eventStyle = (start, end, overlapping, offset) => {
+  const startMinutes = start.get("hour") * 60 + start.get("minute");
+  const endMinutes = end.get("hour") * 60 + end.get("minute");
 
-  let top = (startMinutes/1440)*100;
-  let height = ((endMinutes/1440)*100)-top;
+  let top = (startMinutes / 1440) * 100;
+  let height = (endMinutes / 1440) * 100 - top;
+  let left = 100 - 100 / offset;
   return {
     top: `${top}%`,
-    height: `${height}%`
-  }
-}
+    height: `${height}%`,
+    width: `${100 / overlapping}%`,
+    left: `${left}%`,
+  };
+};
 
-export default function Event({event}) {
+export default function Event({ event }) {
+  const { overlapping, offset } = event;
   return (
-    <div className={styles.event} style={eventStyle(event.start, event.end)}>
-      {event.title} {event.start.format('HH:mm')} - {event.end.format('HH:mm')}
+    <div
+      className={styles.event}
+      style={eventStyle(event.start, event.end, overlapping, offset)}
+    >
+      {event.title} {event.start.format("HH:mm")} - {event.end.format("HH:mm")}
     </div>
-  )
+  );
 }
